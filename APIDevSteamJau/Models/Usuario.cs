0fc73c7f847a49c8dfbdd913259d11d7a8f7bba8
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-04-16 11:58:24 -0300

    Adicionar arquivos de projeto.

---
+++ APIDevSteamJau/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿// Arquivo: Usuario.cs
+
+using Microsoft.AspNetCore.Identity;
+
+namespace APIDevSteamJau.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public Usuario() : base()
+        { }
+
+        public string? NomeCompleto { get; set; }
+        public DateOnly DataNascimento { get; set; }
+    }
+}
